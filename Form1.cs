77c95e20dd8f5a47270691bf65dce9e3c017d5aa
Tarus622 <davideoliveira622@gmail.com>
2022-07-10 01:18:34 -0300

    feat: determinando o vencedor e adicionando mensagem de vitória.

--- Form1.cs
+++ Form1.cs
@@ -107,6 +107,19 @@ namespace MatchingGame
                 secondClicked = clickedLabel;
                 secondClicked.ForeColor = Color.Black;
 
+                // Check to see if the player won
+                CheckForWinner();
+
+                // If the player clicked two matching icons, keep them 
+                // black and reset firstClicked and secondClicked 
+                // so the player can click another icon
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+
                 // If the player gets this far, the player 
                 // clicked two different icons, so start the 
                 // timer (which will wait three quarters of 
@@ -137,6 +150,33 @@ namespace MatchingGame
             // clicked, the program knows it's the first click
             firstClicked = null;
             secondClicked = null;
+        }
+
+        /// <summary>
+        /// Check every icon to see if it is matched, by 
+        /// comparing its foreground color to its background color. 
+        /// If all of the icons are matched, the player wins
+        /// </summary>
+        private void CheckForWinner()
+        {
+            // Go through all of the labels in the TableLayoutPanel, 
+            // checking each one to see if its icon is matched
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+
+            // If the loop didn’t return, it didn't find
+            // any unmatched icons
+            // That means the user won. Show a message and close the form
+            MessageBox.Show("You matched all the icons!", "Congratulations");
+            Close();
         }
     }
 }
